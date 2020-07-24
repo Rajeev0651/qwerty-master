024d0167b1e1e5c8bf481aecc1468f14d84b67e4
Rajeev <singhrajiv0651@gmail.com>
2020-07-24 12:02:45 +0530

    Error message modification

--- client/src/components/Error404/Error404.js
+++ client/src/components/Error404/Error404.js
@@ -3,7 +3,7 @@ import './Error404.css'
 function Error404() {
     return (
         <div className='Error404'>
-            <h1 align='center'><u>404 Page Not Found</u></h1>
+            <h1 align='center'><u>404 Page Not Found !!</u></h1>
         </div>
     )
 }
